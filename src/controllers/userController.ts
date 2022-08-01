6c1cf95eb13894dc76fca87e2952ed57ce191858
fabionicolau <fabionicolaujr@gmail.com>
2022-08-01 17:41:53 -0300

    feat: adiciona validações de username, level, classe e password na rota POST /users

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,16 @@
 import { Response, Request } from 'express';
 import { IUser } from '../interfaces/userInterFace';
+import handleError from '../middlewares/handleError';
 import userService from '../services/userService';
 
 const createUser = async (req: Request, res: Response) => {
-  const user = req.body as IUser;
-  const token = await userService.createUser(user);
-  res.status(201).json({ token });
+  try {
+    const user = req.body as IUser;
+    const token = await userService.createUser(user);
+    res.status(201).json({ token });
+  } catch (error) {
+    handleError(error as Error, req, res);
+  }
 };
 
 export default { createUser };
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,10 +1,32 @@
+import Joi from 'joi';
 import { IUser } from '../interfaces/userInterFace';
 import userModel from '../models/userModel';
 import generateToken from '../utils/jwtCreate';
 
+const validateFields = async ({ username, classe, level, password }: IUser) => {
+  const schema = Joi.object({
+    username: Joi.string().min(3).required().messages({ 'string.empty': '"username" is required',
+      'string.type': '"username" must be a string',
+      'string.min': '"username" length must be at least 3 characters long' }),
+    classe: Joi.string().min(3).required().messages({ 'string.empty': '"classe" is required',
+      'string.type': '"classe" must be a string',
+      'string.min': '"classe" length must be at least 3 characters long' }),
+    level: Joi.number().min(1).required().messages({ 'string.empty': '"level" is required',
+      'number.type': '"level" must be a number',
+      'number.min': '"level" must be greater than or equal to 1' }),
+    password: Joi.string().min(8).required().messages({ 'string.empty': '"password" is required',
+      'string.type': '"password" must be a string',
+      'string.min': '"password" length must be at least 8 characters long' }) }); 
+  
+  const { error, value } = schema.validate({ username, classe, level, password });
+  if (error) throw error;
+  return value; 
+};
+
 const createUser = async (user: IUser): Promise<string> => {
-  await userModel.createUser(user);
-  const token = generateToken(user);
+  const data = await validateFields(user);
+  await userModel.createUser(data);
+  const token = generateToken(data);
   return token;
 };
 
